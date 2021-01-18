8a5aba791c2dedb185b9a646fff33b42cb89132f
taivo911 <taivo.nurk@khk.ee>
2021-01-18 12:25:52 +0200

    M22ratud raamistiku konstandid ja projekti parameetrid

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,14 +1,14 @@
 <?php
 // Project name
-define('PROJECT_NAME', 'projectname');
+define('PROJECT_NAME', 'blog');
 // App root dir
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
 // db params
-define('DB_HOST', 'host');
-define('DB_USER', 'user');
-define('DB_PASS', 'pass');
-define('DB_NAME', 'dbname');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog');
 
