369ffd31d327dadf2bf95b06dfa605a676ea33b4
Taivo <taivo.nurk@khk.ee>
2021-01-21 01:16:27 +0200

    Lisatud rakenduse navigatsiooni kasutajate haldusega seotud elemendid

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,14 @@
+<a href="<?php echo URLROOT; ?>" class="nav-link">Home</a>
+</li>
+</ul>
+<ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+        <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
+    </li>
+    <li class="nav-item">
+        <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
+    </li>
+</ul>
+</div>
+</div>
+</nav>
