38ec22103d521824dfd8f15b76079d095924a974
taivo911 <taivo.nurk@khk.ee>
2021-01-28 10:24:47 +0200

    Loodud sisse logitud kasutaja navigatsiooni riba

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -13,18 +13,15 @@
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
-                <?php if (isset($_SESSION['user_id'])): ?>
                     <li class="nav-item">
                         <a href="<?php echo URLROOT; ?>/users/logout" class="nav-link">Logout</a>
                     </li>
-                <?php else: ?>
                     <li class="nav-item">
                         <a href="<?php echo URLROOT; ?>/users/register" class="nav-link">Register</a>
                     </li>
                     <li class="nav-item">
                         <a href="<?php echo URLROOT; ?>/users/login" class="nav-link">Login</a>
                     </li>
-                <?php endif; ?>
             </ul>
         </div>
     </div>
